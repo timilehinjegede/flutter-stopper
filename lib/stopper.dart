48361775db43f54d4e59f4812c93eaef08f70523
timilehinjegede <timilehintimothy.jegede@gmail.com>
2022-08-01 14:45:34 +0100

    chore: migrate to null safety.

--- lib/stopper.dart
+++ lib/stopper.dart
@@ -8,11 +8,11 @@ typedef Widget StopperBuilder(
     /// A build context
     BuildContext context,
     /// A scroll controller to be passed to a scrollable widget
-    ScrollController controller, 
+    ScrollController? controller, 
     // A scroll physics to be passed to a scrollable widget
-    ScrollPhysics physics, 
+    ScrollPhysics? physics, 
     /// The current stop value.
-    int stop
+    int? stop
   );
 
 /// A widget that changes its height to one of the predefined values based on user-initiated dragging.
@@ -21,7 +21,7 @@ class Stopper extends StatefulWidget {
     /// The list of stop heights in logical pixels. The values must be sorted from lowest to highest.
   final List<double> stops;
     /// This callback function is called when the user triggers a close. If null, the bottom sheet cannot be closed by the user.
-  final Function onClose;
+  final Function? onClose;
     /// A builder to build the contents of the bottom sheet.
   final StopperBuilder builder;
     /// The initial stop.
@@ -29,17 +29,17 @@ class Stopper extends StatefulWidget {
     /// The minimum offset (in logical pixels) necessary to trigger a stop change when dragging.
   final double dragThreshold;
     /// THe desidered shape
-  final ShapeBorder shape;
+  final ShapeBorder? shape;
 
   /// The constructor.
   Stopper({
-    Key key,
-    @required this.builder,
-    @required this.stops,
+    Key? key,
+    required this.builder,
+    required this.stops,
     this.initialStop = 0,
     this.onClose,
     this.dragThreshold = 25,
-    this.shape = null
+    this.shape
   })
       : assert(initialStop < stops.length),
         super(key: key);
@@ -50,18 +50,18 @@ class Stopper extends StatefulWidget {
 
 /// The state of [Stopper] widget.
 class StopperState extends State<Stopper> with SingleTickerProviderStateMixin {
-  List<double> _stops;
-  int _currentStop;
-  int _targetStop;
+  late List<double> _stops;
+  int? _currentStop;
+  int? _targetStop;
   bool _dragging = false;
   bool _closing = false;
-  double _dragOffset;
-  double _closingHeight;
-  ScrollController _scrollController;
-  ScrollPhysics _scrollPhysics;
-  Animation<double> _animation;
-  AnimationController _animationController;
-  Tween<double> _tween;
+  late double _dragOffset;
+  double? _closingHeight;
+  ScrollController? _scrollController;
+  ScrollPhysics? _scrollPhysics;
+  late Animation<double> _animation;
+  late AnimationController _animationController;
+  late Tween<double> _tween;
 
   ScrollPhysics _getScrollPhysicsForStop(s) {
     if (s == _stops.length - 1)
@@ -80,15 +80,15 @@ class StopperState extends State<Stopper> with SingleTickerProviderStateMixin {
     _scrollPhysics = _getScrollPhysicsForStop(_currentStop);
     _animationController = AnimationController(vsync: this, duration: Duration(milliseconds: 500));
     final curveAnimation = CurvedAnimation(parent: _animationController, curve: Curves.linear);
-    _tween = Tween<double>(begin: _stops[_currentStop], end: _stops[_targetStop]);
+    _tween = Tween<double>(begin: _stops[_currentStop!], end: _stops[_targetStop!]);
     _animation = _tween.animate(curveAnimation);
-    _scrollController.addListener(() {
-      if (_scrollController.offset < -widget.dragThreshold) {
+    _scrollController!.addListener(() {
+      if (_scrollController!.offset < -widget.dragThreshold) {
         if (this._currentStop != this._targetStop || _dragging) return;
-        if (this._currentStop > 0) {
+        if (this._currentStop! > 0) {
           final h0 = height;
-          this._targetStop = this._currentStop - 1;
-          _animate(h0, _stops[_targetStop]);
+          this._targetStop = this._currentStop! - 1;
+          _animate(h0, _stops[_targetStop!]);
         } else if (!_closing) {
           close();
         }
@@ -100,15 +100,15 @@ class StopperState extends State<Stopper> with SingleTickerProviderStateMixin {
   void didUpdateWidget(Stopper oldWidget) {
     super.didUpdateWidget(oldWidget);
     this._stops = widget.stops;
-    this._currentStop = min(_currentStop, _stops.length - 1);
-    this._targetStop = min(_currentStop, _stops.length - 1);
+    this._currentStop = min(_currentStop!, _stops.length - 1);
+    this._targetStop = min(_currentStop!, _stops.length - 1);
   }
 
   @override
   void dispose() {
     super.dispose();
     _animationController.dispose();
-    _scrollController.dispose();
+    _scrollController!.dispose();
   }
 
   /// The current stop value. The value changes after the stop change animation is complete.
@@ -131,15 +131,15 @@ class StopperState extends State<Stopper> with SingleTickerProviderStateMixin {
       _animationController.stop(canceled: true);
       _dragging = false;
       _closing = true;
-      widget.onClose();
+      widget.onClose!();
     }
   }
 
-  void _animate(double from, double to, [double velocity]) {
+  void _animate(double? from, double to, [double? velocity]) {
     _tween.begin = from;
     _tween.end = to;
     _animationController.value = 0;
-    if (_scrollController.offset < 0) _scrollController.animateTo(0, duration: Duration(milliseconds: 200), curve: Curves.linear);
+    if (_scrollController!.offset < 0) _scrollController!.animateTo(0, duration: Duration(milliseconds: 200), curve: Curves.linear);
     _animationController.fling().then((_) {
       this._currentStop = this._targetStop;
       setState(() {
@@ -153,11 +153,11 @@ class StopperState extends State<Stopper> with SingleTickerProviderStateMixin {
     if (_closing)
       return _closingHeight;
     else if (_dragging)
-      return _stops[_currentStop] + _dragOffset;
+      return _stops[_currentStop!] + _dragOffset;
     else if (_animationController.isAnimating)
       return _animation.value;
     else
-      return _stops[_currentStop];
+      return _stops[_currentStop!];
   }
 
   @override
@@ -167,14 +167,14 @@ class StopperState extends State<Stopper> with SingleTickerProviderStateMixin {
         child: GestureDetector(
           onVerticalDragStart: (details) {
             if (_currentStop != _targetStop) return;
-            _scrollController.jumpTo(0);
+            _scrollController!.jumpTo(0);
             _dragging = true;
             _dragOffset = 0;
             setState(() {});
           },
           onVerticalDragUpdate: (details) {
             if (_dragging) {
-              _scrollController.jumpTo(0);
+              _scrollController!.jumpTo(0);
               _dragOffset -= details.delta.dy;
               setState(() {});
             }
@@ -182,15 +182,15 @@ class StopperState extends State<Stopper> with SingleTickerProviderStateMixin {
           onVerticalDragEnd: (details) {
             if (!_dragging || _closing) return;
             if (_dragOffset > widget.dragThreshold) {
-              _targetStop = min(_currentStop + 1, _stops.length - 1);
+              _targetStop = min(_currentStop! + 1, _stops.length - 1);
             } else if (_dragOffset < -widget.dragThreshold) {
-              _targetStop = max(canClose ? -1 : 0, _currentStop - 1);
+              _targetStop = max(canClose ? -1 : 0, _currentStop! - 1);
             }
-            if (_targetStop < 0) {
+            if (_targetStop! < 0) {
               close();
             } else {
               _dragging = false;
-              _animate(_stops[_currentStop] + _dragOffset, _stops[_targetStop]);
+              _animate(_stops[_currentStop!] + _dragOffset, _stops[_targetStop!]);
             }
           },
           child: widget.builder(context, _scrollController, _scrollPhysics, _currentStop),
@@ -209,14 +209,14 @@ class StopperState extends State<Stopper> with SingleTickerProviderStateMixin {
 PersistentBottomSheetController showStopper(
     {
     /// The key of the [Stopper] widget
-    Key key,
+    Key? key,
     /// The build context 
-    @required BuildContext context,
+    required BuildContext context,
     /// The builder of the bottom sheet
-    @required StopperBuilder builder,
+    required StopperBuilder builder,
     /// The list of stop heights as logical pixel values. Use [MediaQuery] to compute the heights relative to screen height.
     /// The order of the stop heights must be from the lowest to the highest.
-    @required List<double> stops,
+    required List<double> stops,
     /// The initial stop number.
     int initialStop = 0,
     /// If [true] then the user can close the bottom sheet dragging it down from the lowest stop. 
@@ -224,11 +224,11 @@ PersistentBottomSheetController showStopper(
     /// The minimum offset (in logical pixels) to trigger a stop change when dragging.
     double dragThreshold = 25,
     /// The desidered shape
-    ShapeBorder shape = null,
+    ShapeBorder? shape,
     /// Required for modal bottomSheet
     bool isScrollController = true
     }) {
-  PersistentBottomSheetController cont;
+  late PersistentBottomSheetController cont;
   cont = showBottomSheet(
     context: context,
     builder: (context) {
@@ -253,14 +253,14 @@ PersistentBottomSheetController showStopper(
 Future showModalStopper(
     {
       /// The key of the [Stopper] widget
-      Key key,
+      Key? key,
       /// The build context
-      @required BuildContext context,
+      required BuildContext context,
       /// The builder of the bottom sheet
-      @required StopperBuilder builder,
+      required StopperBuilder builder,
       /// The list of stop heights as logical pixel values. Use [MediaQuery] to compute the heights relative to screen height.
       /// The order of the stop heights must be from the lowest to the highest.
-      @required List<double> stops,
+      required List<double> stops,
       /// The initial stop number.
       int initialStop = 0,
       /// If [true] then the user can close the bottom sheet dragging it down from the lowest stop.
@@ -268,7 +268,7 @@ Future showModalStopper(
       /// The minimum offset (in logical pixels) to trigger a stop change when dragging.
       double dragThreshold = 25,
       /// The desidered shape
-      ShapeBorder shape = null,
+      ShapeBorder? shape,
       /// Required for modal bottomSheet
       bool isScrollController = true,
     }) {
